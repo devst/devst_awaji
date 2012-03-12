fb0b835ff049f4c86611387b461cd960f1fb3b7c
Kamiya Atsuteru <teru.k.business@gmail.com>
2012-03-13 02:55:28 +0900

    move answer interfaces to devst_awaji_answers.

--- app/models/Question1.java
+++ app/models/Question1.java
@@ -3,7 +3,6 @@ package models;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import answers.Answer1;
@@ -12,11 +11,6 @@ import answers.Answer1;
 public class Question1 {
 
 	private Answer1 sut;
-
-	@Before
-	public void setUp() {
-		sut = new answers.a.Answer1();
-	}
 
 	@Test
 	public void test1() {
