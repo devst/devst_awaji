7579a7da06be722267e8418a549b57fe636039b3
irof <@irof>
2012-03-22 01:45:45 +0900

    Judgeクラスの使用

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,10 +2,13 @@ package controllers;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+
+import judges.Judge;
 
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
@@ -19,29 +22,24 @@ import features.Index.Entry;
 public class Application extends Controller {
 
 	/**
-	 * スコアボードに表示するインタフェースのリスト
-	 */
-	private static List<Entry> index = Index.getList();
-
-	/**
 	 * スコアボード表示
 	 */
 	public static void index() {
-		Map<String, Map<String, String>> resultMap = createResultMap();
+		Map<Team, Map<String, String>> resultMap = createResultMap();
 		render(resultMap);
 	}
 
-	protected static Map<String, Map<String, String>> createResultMap() {
+	protected static Map<Team, Map<String, String>> createResultMap() {
 
-		Map<String, Map<String, String>> resultMap = new LinkedHashMap<String, Map<String, String>>();
+		Map<Team, Map<String, String>> resultMap = new EnumMap<Team, Map<String, String>>(Team.class);
 		for (Team team : Team.values()) {
 			Map<String, String> scoreMap = new LinkedHashMap<String, String>();
 
-			for (Entry e : index) {
+			for (Entry e : Index.getList()) {
 				String testResult = test(e.judgeman, team.getFeature(e.feature));
 				scoreMap.put(e.name, testResult);
 			}
-			resultMap.put(team.teamName, scoreMap);
+			resultMap.put(team, scoreMap);
 		}
 		return resultMap;
 	}
@@ -60,10 +58,8 @@ public class Application extends Controller {
 			result = new JUnitCore().run(new BlockJUnit4ClassRunner(test) {
 				@Override
 				protected Object createTest() throws Exception {
-					Object obj = super.createTest();
-					Field field = getTestClass().getJavaClass().getDeclaredField("sut");
-					field.setAccessible(true);
-					field.set(obj, answer.newInstance());
+					Judge obj = (Judge) super.createTest();
+					obj.sut = answer.newInstance();
 					return obj;
 				}
 			});
