ae4b5d1c6a717981a9ea2d56064e1eda73878075
irof <irof@javaring.info>
2012-03-28 22:20:03 +0900

    BingoTestSuite

---
+++ test/internal/BingoTestSuite.java
@@ -0,0 +1,10 @@
+package internal;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BingoCellTest.class, BingoTest.class })
+public class BingoTestSuite {
+}
